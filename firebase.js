b8d0a0a3de7274a40c94cdf2e62a95178d2cda8f
JKim0529 <57051199+JKim0529@users.noreply.github.com>
2023-10-03 18:07:20 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCU07I8xJkyuyfRSTO-VP9OSH10X9qRJeA",
-    authDomain: "todoapp-ad7bb.firebaseapp.com",
-    projectId: "todoapp-ad7bb",
-    storageBucket: "todoapp-ad7bb.appspot.com",
-    messagingSenderId: "804215005601",
-    appId: "1:804215005601:web:e0ad57621b5ba36e93ebe0",
-    measurementId: "G-M8VZ9BKWTW"
+    apiKey: API_KEY,
+    authDomain: AUTH-DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGE_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
